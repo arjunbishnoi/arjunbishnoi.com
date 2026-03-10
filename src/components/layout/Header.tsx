0264916a95be9b6eaddda59ba27d4fc18bdd1b31
Arjun <arjunbishnoi1@gmail.com>
2026-03-10 13:07:09 -0400

    feat: add Header component with responsive navigation, theme toggle, and scroll effects.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -339,7 +339,7 @@ export function Header() {
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0 }}
                     transition={{ duration: 0.2 }}
-                    className="md:hidden pl-7 pr-4 pt-2 pb-9 w-full flex flex-col"
+                    className="md:hidden pl-7 pr-4 pt-2 pb-[22px] w-full flex flex-col"
                 >
                     <nav className="flex flex-col space-y-3">
                         {[
@@ -387,7 +387,7 @@ export function Header() {
 
                     {/* Social Icons Row */}
                     <motion.div 
-                        className="flex items-center space-x-6 mt-6 pl-1 pt-1 opacity-90"
+                        className="flex items-center gap-[17px] mt-3 opacity-90 -ml-[6px]"
                         initial={{ opacity: 0, scale: 0.96, y: -10 }}
                         animate={{ opacity: 1, scale: 1, y: 0 }}
                         exit={{ opacity: 0, scale: 0.96, y: -10 }}
@@ -400,11 +400,11 @@ export function Header() {
                     >
                         <a 
                             href="mailto:arjunbishnoi@gmail.com" 
-                            className="text-zinc-500 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-100 transition-colors"
+                            className="flex items-center justify-center w-10 h-10 rounded-full text-zinc-500 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-100 transition-colors"
                             aria-label="Email"
                             onClick={() => setIsMobileMenuOpen(false)}
                         >
-                            <svg viewBox="0 0 24 24" className="w-[1.8rem] h-[1.8rem] fill-current" xmlns="http://www.w3.org/2000/svg">
+                            <svg viewBox="0 0 24 24" className="w-[1.95rem] h-[1.95rem] fill-current" xmlns="http://www.w3.org/2000/svg">
                                 <path fillRule="evenodd" clipRule="evenodd" d="M7 2.75C5.38503 2.75 3.92465 3.15363 2.86466 4.1379C1.79462 5.13152 1.25 6.60705 1.25 8.5V15.5C1.25 17.393 1.79462 18.8685 2.86466 19.8621C3.92465 20.8464 5.38503 21.25 7 21.25H17C18.615 21.25 20.0754 20.8464 21.1353 19.8621C22.2054 18.8685 22.75 17.393 22.75 15.5V8.5C22.75 6.60705 22.2054 5.13152 21.1353 4.1379C20.0754 3.15363 18.615 2.75 17 2.75H7ZM19.2285 8.3623C19.5562 8.10904 19.6166 7.63802 19.3633 7.31026C19.1101 6.98249 18.6391 6.9221 18.3113 7.17537L12.7642 11.4616C12.3141 11.8095 11.6858 11.8095 11.2356 11.4616L5.6886 7.17537C5.36083 6.9221 4.88982 6.98249 4.63655 7.31026C4.38328 7.63802 4.44367 8.10904 4.77144 8.3623L10.3185 12.6486C11.3089 13.4138 12.691 13.4138 13.6814 12.6486L19.2285 8.3623Z" />
                             </svg>
                         </a>
@@ -412,11 +412,11 @@ export function Header() {
                             href="https://www.behance.net/" 
                             target="_blank" 
                             rel="noopener noreferrer" 
-                            className="text-zinc-500 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-100 transition-colors"
+                            className="flex items-center justify-center w-10 h-10 rounded-full text-zinc-500 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-100 transition-colors"
                             aria-label="Behance"
                             onClick={() => setIsMobileMenuOpen(false)}
                         >
-                            <svg viewBox="-40 -40 3413 3413" className="w-[1.8rem] h-[1.8rem] fill-current" xmlns="http://www.w3.org/2000/svg">
+                            <svg viewBox="-40 -40 3413 3413" className="w-[1.875rem] h-[1.875rem] fill-current" xmlns="http://www.w3.org/2000/svg">
                                 <path fillRule="evenodd" clipRule="evenodd" d="M1667 0c920 0 1667 746 1667 1667 0 920-746 1667-1667 1667C747 3334 0 2588 0 1667 0 747 746 0 1667 0zm-408 1059c57 0 109 5 156 15s87 27 121 49c33 23 59 53 78 91 18 37 27 85 27 140 0 60-14 110-41 151-28 40-68 73-122 99 74 21 128 58 164 111s54 117 54 192c0 61-12 113-35 157-24 44-55 80-94 108s-85 49-136 62c-50 13-102 20-156 20H696V1060h563zm704 96h484v118h-484v-118zm108 890c36 35 87 52 154 52 48 0 90-12 124-36s55-50 63-77h209c-34 104-85 178-154 223s-153 67-250 67c-68 0-129-11-184-33s-101-53-140-93c-38-40-67-88-88-144-20-56-31-118-31-184 0-65 11-125 32-181 22-56 51-104 91-145 39-41 86-73 140-96 54-24 114-35 181-35 73 0 137 14 192 43 55 28 100 67 135 115s60 103 76 164 21 125 17 193h-624c0 68 23 133 59 167zm273-454c-28-31-76-48-134-48-38 0-69 6-94 19s-45 29-60 48-26 39-32 61c-6 21-10 40-11 57h387c-6-61-27-105-55-137zm-1118-50c47 0 85-11 116-33 30-22 45-58 45-108 0-28-5-51-15-69s-24-32-41-42-36-17-58-21-44-6-67-6H960v279h266zm14 508c26 0 50-2 73-8 24-5 44-13 62-25 17-12 32-27 43-48 11-20 16-46 16-77 0-61-17-105-52-132-34-26-80-39-137-39H960v329h281v1z" />
                             </svg>
                         </a>
@@ -424,11 +424,11 @@ export function Header() {
                             href={socialLinks.github} 
                             target="_blank" 
                             rel="noopener noreferrer" 
-                            className="text-zinc-500 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-100 transition-colors"
+                            className="flex items-center justify-center w-10 h-10 rounded-full text-zinc-500 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-100 transition-colors"
                             aria-label="GitHub"
                             onClick={() => setIsMobileMenuOpen(false)}
                         >
-                            <svg viewBox="0 0 24 24" className="w-[1.8rem] h-[1.8rem] fill-current" xmlns="http://www.w3.org/2000/svg">
+                            <svg viewBox="0 0 24 24" className="w-[1.875rem] h-[1.875rem] fill-current" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
                             </svg>
                         </a>
@@ -436,11 +436,11 @@ export function Header() {
                             href={socialLinks.linkedin} 
                             target="_blank" 
                             rel="noopener noreferrer" 
-                            className="text-zinc-500 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-100 transition-colors"
+                            className="flex items-center justify-center w-10 h-10 rounded-full text-zinc-500 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-100 transition-colors"
                             aria-label="LinkedIn"
                             onClick={() => setIsMobileMenuOpen(false)}
                         >
-                            <svg viewBox="0 0 24 24" className="w-[1.8rem] h-[1.8rem] fill-current" xmlns="http://www.w3.org/2000/svg">
+                            <svg viewBox="0 0 24 24" className="w-[1.875rem] h-[1.875rem] fill-current" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
                             </svg>
                         </a>
